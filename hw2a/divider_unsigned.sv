d3ed3e8a9775d263ea5402a4177236cc89ede560
aabdel0181 <ahmedabdellahhoco@gmail.com>
2024-02-19 17:24:59 +0000

    Need CLA

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -13,28 +13,29 @@ module divider_unsigned (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-    // TODO: your code here
-    wire[31:0] store_dividend[33];
-    wire[31:0] store_quotient[33];
-    wire[31:0] store_remainder[33];
-    assign store_dividend[0] = i_dividend;
-    assign store_quotient[0] = 32'h0000_0000;
-    assign store_remainder[0] = 32'h0000_0000;
-    genvar i;
-    generate for (i = 0; i < 32; i = i + 1) begin : div_iteration
-        divu_1iter div_mod(
-            .i_dividend(store_dividend[i]),
-            .i_divisor(i_divisor),
-            .i_remainder(store_remainder[i]),
-            .i_quotient(store_quotient[i]),
-            .o_dividend(store_dividend[i+1]),
-            .o_remainder(store_remainder[i+1]),
-            .o_quotient(store_quotient[i+1])
-        ); 
-    end    
-    endgenerate
-    assign o_remainder = store_remainder[32];
-    assign o_quotient = store_quotient[32];
+  // TODO: your code here
+  wire [31:0] store_dividend [33];
+  wire [31:0] store_quotient [33];
+  wire [31:0] store_remainder[33];
+  assign store_dividend[0]  = i_dividend;
+  assign store_quotient[0]  = 32'h0000_0000;
+  assign store_remainder[0] = 32'h0000_0000;
+  genvar i;
+  generate
+    for (i = 0; i < 32; i = i + 1) begin : div_iteration
+      divu_1iter div_mod (
+          .i_dividend (store_dividend[i]),
+          .i_divisor  (i_divisor),
+          .i_remainder(store_remainder[i]),
+          .i_quotient (store_quotient[i]),
+          .o_dividend (store_dividend[i+1]),
+          .o_remainder(store_remainder[i+1]),
+          .o_quotient (store_quotient[i+1])
+      );
+    end
+  endgenerate
+  assign o_remainder = store_remainder[32];
+  assign o_quotient  = store_quotient[32];
 endmodule
 
 
@@ -59,12 +60,14 @@ module divu_1iter (
         dividend = dividend << 1;
     }
     */
-    // TODO: your code here
-    wire [31:0] intermediate;
-    wire [0:0] less_than;
-    assign intermediate = (i_remainder << 1) | ((i_dividend >> 31) & 32'h0000_0001);
-    assign less_than = intermediate < i_divisor;
-    assign o_remainder = less_than ? intermediate : (intermediate - i_divisor);
-    assign o_quotient = less_than ? (i_quotient << 1) : ((i_quotient << 1) | 32'h0000_0001);
-    assign o_dividend = i_dividend << 1;
+  // TODO: your code here
+  wire [31:0] intermediate;
+  wire [ 0:0] less_than;
+
+  assign intermediate = (i_remainder << 1) | ((i_dividend >> 31) & 32'h0000_0001);
+  assign less_than = intermediate < i_divisor;
+  assign o_remainder = less_than ? intermediate : (intermediate - i_divisor);
+  assign o_quotient = less_than ? (i_quotient << 1) : ((i_quotient << 1) | 32'h0000_0001);
+  assign o_dividend = i_dividend << 1;
+
 endmodule
--- hw2b/cla.sv
+++ hw2b/cla.sv
@@ -6,10 +6,14 @@
  * @param g whether a and b generate a carry
  * @param p whether a and b would propagate an incoming carry
  */
-module gp1(input wire a, b,
-           output wire g, p);
-   assign g = a & b;
-   assign p = a | b;
+module gp1 (
+    input  wire a,
+    b,
+    output wire g,
+    p
+);
+  assign g = a & b;
+  assign p = a | b;
 endmodule
 
 /**
@@ -21,30 +25,100 @@ endmodule
  * @param pout whether these 4 bits internally would propagate an incoming carry from cin
  * @param cout the carry outs for the low-order 3 bits
  */
-module gp4(input wire [3:0] gin, pin,
-           input wire cin,
-           output wire gout, pout,
-           output wire [2:0] cout);
+module gp4 (
+    input wire [3:0] gin,
+    pin,
+    input wire cin,
+    output wire gout,
+    pout,
+    output wire [2:0] cout
+);
+  wire inter1, inter2, inter3;
 
-   // TODO: your code here
+  //assign carry out bits 
+  assign inter1 = (cin & pin[0]) | gin[0];
+  assign inter2 = (pin[1] & inter1) | gin[1];
+  assign inter3 = (pin[2] & inter2) | gin[2];
+
+  assign cout[0] = inter1;
+  assign cout[1] = inter2;
+  assign cout[2] = inter3;
+
+  //propogate 
+  assign pout = pin[0] & pin[1] & pin[2] & pin[3];
+
+  //generate 
+  assign gout = (gin[0] & gin[1] & pin[2] & pin[3]) | (gin[2] | gin[3]);
 
 endmodule
 
 /** Same as gp4 but for an 8-bit window instead */
-module gp8(input wire [7:0] gin, pin,
-           input wire cin,
-           output wire gout, pout,
-           output wire [6:0] cout);
+module gp8 (
+    input wire [7:0] gin,
+    pin,
+    input wire cin,
+    output wire gout,
+    pout,
+    output wire [6:0] cout
+);
 
-   // TODO: your code here
+  // TODO: your code here
+  wire inter1, inter2, inter3, inter4, inter5, inter6, inter7;
 
+  //assign carry out bits 
+  assign inter1 = (cin & pin[0]) | gin[0];
+  assign inter2 = (pin[1] & inter1) | gin[1];
+  assign inter3 = (pin[2] & inter2) | gin[2];
+  assign inter4 = (pin[3] & inter3) | gin[3];
+  assign inter5 = (pin[4] & inter4) | gin[4];
+  assign inter6 = (pin[5] & inter5) | gin[5];
+  assign inter7 = (pin[6] & inter6) | gin[6];
+  assign cout[0] = inter1;
+  assign cout[1] = inter2;
+  assign cout[2] = inter3;
+  assign cout[3] = inter4;
+  assign cout[4] = inter5;
+  assign cout[5] = inter6;
+  assign cout[6] = inter7;
+
+
+  //propogate 
+  assign pout = pin[0] & pin[1] & pin[2] & pin[3] & pin[4] & pin[5] & pin[6] & pin[7];
+
+  //generate 
+  assign gout = gin[7] | 
+              (gin[6] & pin[7]) | 
+              (gin[5] & pin[6] & pin[7]) | 
+              (gin[4] & (|pin[7:5])) | 
+              (gin[3] & (|pin[7:4])) |
+              (gin[2] & (|pin[7:3])) |
+              (gin[1] & (|pin[7:2])) |
+              (gin[0] & (|pin[7:1]));
 endmodule
 
-module cla
-  (input wire [31:0]  a, b,
-   input wire         cin,
-   output wire [31:0] sum);
+module cla (
+    input  wire [31:0] a,
+    b,
+    input  wire        cin,
+    output wire [31:0] sum
+);
 
-   // TODO: your code here
+  // TODO: your code here
+
+  // First lets generate our 32 gp1 modules
+
+  genvar gp1;
+  generate
+    for (gp1 = 0; gp1 < 32; gp1++) begin
+      gp1 new_gp1 (
+          .a(a[i]),
+          .b(b[i]),
+          .g(g[i]),
+          .p(p[i])
+      );
+    end
+  endgenerate
+
+
 
 endmodule
