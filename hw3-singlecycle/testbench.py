3397e4bdb032b320366846275ae66e7dcc85a844
rushil-r <rushilr@sas.upenn.edu>
2024-02-23 17:16:59 +0000

    all the riscV tests that are in pybench pass, but not official riscV

--- hw3-singlecycle/testbench.py
+++ hw3-singlecycle/testbench.py
@@ -281,7 +281,7 @@ async def testEcall(dut):
 @cocotb.test()
 async def testOneRiscvTest(dut):
     "Use this to run one particular riscv test"
-    await riscvTest(dut, RISCV_TESTS_PATH / 'rv32ui-p-simple')
+    await riscvTest(dut, RISCV_TESTS_PATH / 'rv32ui-p-lui')
 
 async def riscvTest(dut, binaryPath=None):
     "Run the official RISC-V test whose binary lives at `binaryPath`"
