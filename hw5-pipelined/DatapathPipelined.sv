57295e6e7b62fe5f70219af3d6b27e8a585384ff
rushil-r <rushilr@sas.upenn.edu>
2024-03-30 03:01:07 +0000

    merged more of our old code

--- hw5-pipelined/DatapathPipelined.sv
+++ hw5-pipelined/DatapathPipelined.sv
@@ -118,6 +118,55 @@ module DatapathPipelined (
     output cycle_status_e trace_writeback_cycle_status
 );
 
+  logic [0:0] regfile_we;
+  logic [`REG_SIZE] data_rd;
+  logic [`REG_SIZE] data_rs1;
+  logic [`REG_SIZE] data_rs2;
+  logic [4:0] regfile_rd;
+  logic [4:0] regfile_rs1;
+  logic [4:0] regfile_rs2;
+
+  // components of the instruction
+  wire [6:0] insn_funct7;
+  wire [4:0] insn_rs2;
+  wire [4:0] insn_rs1;
+  wire [2:0] insn_funct3;
+  wire [4:0] insn_rd;
+  wire [`OPCODE_SIZE] insn_opcode;
+  // split R-type instruction - see section 2.2 of RiscV spec
+  assign {insn_funct7, insn_rs2, insn_rs1, insn_funct3, insn_rd, insn_opcode} = insn_from_imem;
+
+  // setup for I, S, B & J type instructions
+  // I - short immediates and loads
+  wire [11:0] imm_i;
+  assign imm_i = insn_from_imem[31:20];
+  wire [ 4:0] imm_shamt = insn_from_imem[24:20];
+
+  // S - stores
+  wire [11:0] imm_s;
+  assign imm_s[11:5] = insn_funct7, imm_s[4:0] = insn_rd;
+
+  // B - conditionals
+  wire [12:0] imm_b;
+  assign {imm_b[12], imm_b[10:5]} = insn_funct7, {imm_b[4:1], imm_b[11]} = insn_rd, imm_b[0] = 1'b0;
+
+  // J - unconditional jumps
+  wire [20:0] imm_j;
+  assign {imm_j[20], imm_j[10:1], imm_j[11], imm_j[19:12], imm_j[0]} = {
+    insn_from_imem[31:12], 1'b0
+  };
+
+  // U - 20-bit immediate
+  wire [19:0] imm_u;
+  assign imm_u = insn_from_imem[31:12];
+
+
+  wire [`REG_SIZE] imm_i_sext = {{20{imm_i[11]}}, imm_i[11:0]};
+  wire [`REG_SIZE] imm_s_sext = {{20{imm_s[11]}}, imm_s[11:0]};
+  wire [`REG_SIZE] imm_b_sext = {{19{imm_b[12]}}, imm_b[12:0]};
+  wire [`REG_SIZE] imm_j_sext = {{11{imm_j[20]}}, imm_j[20:0]};
+  wire [`REG_SIZE] imm_u_sext = {{12{imm_u[19]}}, imm_u[19:0]};
+
   // opcodes - see section 19 of RiscV spec
   localparam bit [`OPCODE_SIZE] OpcodeLoad = 7'b00_000_11;
   localparam bit [`OPCODE_SIZE] OpcodeStore = 7'b01_000_11;
